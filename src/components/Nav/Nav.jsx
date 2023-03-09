686d66253f0ce8c76c5b1487f7fccb25e87e8848
John Mayou <johnm.ceo@gmail.com>
2023-03-09 10:59:49 -0600

    navbar styling, reset anchor tag styling on global styles

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -8,34 +8,36 @@ function Nav() {
 	const user = useSelector((store) => store.user);
 
 	return (
-		<div className="nav">
-			<Link to="/home">
-				<h2 className="nav-title">Auth Shelf</h2>
-			</Link>
-			<div>
-				{/* If no user is logged in, show these links */}
-				{user.id === null && (
-					// If there's no user, show login/registration links
-					<Link className="navLink" to="/login">
-						Login / Register
-					</Link>
-				)}
-
-				{/* If a user is logged in, show these links */}
-				{user.id && (
-					<>
-						<Link className="navLink" to="/shelf">
-							The Shelf
-						</Link>
-						<LogOutButton className="navLink" />
-					</>
-				)}
-
-				<Link className="navLink" to="/about">
-					About
+		<section>
+			<div className="nav">
+				<Link className="nav__link" to="/home">
+					<h2 className="nav__title">Auth Shelf</h2>
 				</Link>
+				<div>
+					{/* If no user is logged in, show these links */}
+					{user.id === null && (
+						// If there's no user, show login/registration links
+						<Link className="nav__link" to="/login">
+							Login / Register
+						</Link>
+					)}
+
+					{/* If a user is logged in, show these links */}
+					{user.id && (
+						<>
+							<Link className="nav__link" to="/shelf">
+								The Shelf
+							</Link>
+							<LogOutButton className="navLink" />
+						</>
+					)}
+
+					<Link className="nav__link" to="/about">
+						About
+					</Link>
+				</div>
 			</div>
-		</div>
+		</section>
 	);
 }
 
