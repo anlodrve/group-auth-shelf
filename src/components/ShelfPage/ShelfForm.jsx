9eea8e833360991bc6b42dc54928e4c4b27107c9
John Mayou <johnm.ceo@gmail.com>
2023-03-09 17:15:49 -0600

    form conditional

--- src/components/ShelfPage/ShelfForm.jsx
+++ src/components/ShelfPage/ShelfForm.jsx
@@ -1,28 +1,48 @@
-import { useState } from "react"
-import { useDispatch } from "react-redux"
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 function ShelfForm() {
-	const [shelfItem, setShelfItem] = useState({})
-	const dispatch = useDispatch()
+	const user = useSelector((store) => store.user);
+	const [shelfItem, setShelfItem] = useState({});
+	const dispatch = useDispatch();
 
 	const submitForm = () => {
 		dispatch({
-			type: 'ADD_ITEM',
-			payload: shelfItem
-		})
-	}
+			type: "ADD_ITEM",
+			payload: shelfItem,
+		});
+	};
 
 	return (
-	<>
-		<h1>Shelf Form</h1>
-		<form onSubmit={submitForm}>
-			<input onChange={(event) => setShelfItem({...shelfItem, image_url: event.target.value})} placeholder="image url"/>
-			<input onChange={(event) => setShelfItem({...shelfItem, description: event.target.value})} placeholder="description"/>
-			<button type="submit">Submit</button>
-		</form>
-	</>
-	)
+		<>
+			{user.id && (
+				<>
+					<h1>Shelf Form</h1>
+					<form onSubmit={submitForm}>
+						<input
+							onChange={(event) =>
+								setShelfItem({
+									...shelfItem,
+									image_url: event.target.value,
+								})
+							}
+							placeholder="image url"
+						/>
+						<input
+							onChange={(event) =>
+								setShelfItem({
+									...shelfItem,
+									description: event.target.value,
+								})
+							}
+							placeholder="description"
+						/>
+						<button type="submit">Submit</button>
+					</form>
+				</>
+			)}
+		</>
+	);
 }
 
 export default ShelfForm;
-
