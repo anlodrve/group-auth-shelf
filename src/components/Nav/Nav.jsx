0354dad795099cf5322039c0ef5a19ad9e56819e
John Mayou <johnm.ceo@gmail.com>
2023-03-09 12:29:53 -0600

    update nav styling, clearing out landing page boilerplate css

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,37 +7,36 @@ import { useSelector } from "react-redux";
 function Nav() {
 	const user = useSelector((store) => store.user);
 
+	console.log(user);
 	return (
-		<section>
-			<div className="nav">
-				<Link className="nav__link" to="/home">
-					<h2 className="nav__title">Auth Shelf</h2>
-				</Link>
-				<div>
-					{/* If no user is logged in, show these links */}
-					{user.id === null && (
-						// If there's no user, show login/registration links
-						<Link className="nav__link" to="/login">
-							Login / Register
-						</Link>
-					)}
-
-					{/* If a user is logged in, show these links */}
-					{user.id && (
-						<>
-							<Link className="nav__link" to="/shelf">
-								The Shelf
-							</Link>
-							<LogOutButton className="navLink" />
-						</>
-					)}
-
-					<Link className="nav__link" to="/about">
-						About
+		<div className="nav">
+			<Link className="nav__title-link" to="/home">
+				<h2 className="nav__title">Auth Shelf</h2>
+			</Link>
+			<div className="nav__link-box">
+				{/* If no user is logged in, show these links */}
+				{(user.id === null || !user.id) && (
+					// If there's no user, show login/registration links
+					<Link className="nav__link" to="/login">
+						Login / Register
 					</Link>
-				</div>
+				)}
+
+				{/* If a user is logged in, show these links */}
+				{user.id && (
+					<>
+						<Link className="nav__link" to="/shelf">
+							The Shelf
+						</Link>
+						<LogOutButton className="navLink" />
+					</>
+				)}
+
+				<Link className="nav__link" to="/about">
+					About
+				</Link>
 			</div>
-		</section>
+		</div>
 	);
 }
 
