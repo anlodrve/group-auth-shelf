b3ed16871491de5828428746a4e905ba0db23eb5
John Mayou <johnm.ceo@gmail.com>
2023-03-09 10:21:54 -0600

    initial global style resets

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -20,7 +20,7 @@ import LandingPage from "../LandingPage/LandingPage";
 import LoginPage from "../LoginPage/LoginPage";
 import RegisterPage from "../RegisterPage/RegisterPage";
 
-import "./App.css";
+import "./App.scss";
 
 function App() {
 	const dispatch = useDispatch();
