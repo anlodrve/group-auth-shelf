56acd698c82138dac515634c73eb7aeacf3234de
Andrea Love <andrealovelovelove@gmail.com>
2023-03-09 10:00:57 -0600

    added Delete button, watcher saga for delete, generator function for delete

--- src/components/ShelfPage/ShelfItem.jsx
+++ src/components/ShelfPage/ShelfItem.jsx
@@ -1,5 +1,35 @@
 function ShelfItem() {
-	return <h1>Shelf Item</h1>;
+
+
+	const handleDelete = () => {
+		dispatchEvent({
+			type: 'DELETE_ITEM',
+			payload: id
+		})
+	}
+
+	// for the watcher sgag
+	yield takeEvery('DELETE_ITEM', deleteItem)
+
+	// delete generator function - action.payload is the id
+	function* deleteItem(action) {
+		try {
+			yield axios.delete(`/api/shelf/${action.payload}`)
+			//double check the get dispatch name 
+			yield put({type: 'GET_ITEMS'})
+		} catch (error) {
+			
+		}
+	}
+
+
+
+	return ( 
+	<>
+		<h1>Shelf Item</h1>
+			<button className="deleteButton" onClick={handleDelete()}>Delete</button>
+	</>
+	)
 }
 
 export default ShelfItem;
